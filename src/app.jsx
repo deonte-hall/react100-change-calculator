bbfc938e7b96d018780250fce2ec0e2487e60305
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-06-17 11:25:55 -0700

    This is -v2 for state values

--- src/app.jsx
+++ src/app.jsx
@@ -2,34 +2,61 @@
 import React, { Component } from 'react';
 import { useState } from 'react';
 
-
+ useState
 class App extends Component {
+
+    //Used constructor to set initial state values of each output 
   constructor(props) {
     super(props);
-    this.state = {amountDue: "", amountPaid: "", result: "" }
+    this.state = {
+        amountDue: "", 
+        amountPaid: "", 
+        result: "",
+    twentyBills: "",
+    tenBills: "",
+    fiveBills: "",
+    oneBills: "",
+    quarterCoins: "",
+    dimeCoins: "",
+    nickelCoins: "",
+    pennyCoins: ""
+
+}
     this.calculate = this.calculate.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    //this.stateChanger = this.stateChanger.bind(this);
   }
-
   
-  calculate(due, paid){
-      console.log("this is log", due, paid)
-    
-    let changeOwed = 0;
-    let theirAmount =0;
-    let twentyBills = 0;
-    let tenBills =0;
-    let fiveBills =0;
-    //let twoBills = 0;
-    let oneBills = 0;
-    let quarterCoins =0;
-    let dimeCoins =0;
+  calculate(due, paid = null){
+   
+//Declaring values of all bill denominations and the entire result 
+
+
+    let changeOwed=0;
+    let theirAmount=0;
+    let twentyBills=0;
+    let tenBills=0;
+    let fiveBills=0;
+
+    let oneBills=0;
+    let quarterCoins=0;
+    let dimeCoins=0;
     let nickelCoins=0;
     let pennyCoins=0;
+
+      console.log("this is log", due, paid)
+      
+    
+
+    
         if(paid >= due){
-                changeOwed = paid - due;
-                theirAmount = Math.abs(paid - due);
+                changeOwed = paid - due
+                theirAmount = Math.abs(paid - due)
+                console.log("their-amount:", theirAmount, "change-owed:", changeOwed)
+
         }
+
+//Series of if statements to filter changeOwed and separate each bill and coin denomination
         if(changeOwed >= 20){
             while(changeOwed >= 20){
                 twentyBills++;
@@ -55,7 +82,6 @@ class App extends Component {
         //     }
         // }
         if(changeOwed >= 1){
-          console.log(changeOwed)
             while(changeOwed >= 1){
                 oneBills++;
                 changeOwed -= 1;
@@ -79,10 +105,8 @@ class App extends Component {
                 changeOwed -= 0.05;
             }
         }
-
         changeOwed = changeOwed.toFixed(2);
         if(changeOwed > 0.00){
-            console.log(changeOwed);
             while(changeOwed > 0.00){
                 pennyCoins++;
                 changeOwed -= 0.01;
@@ -93,10 +117,22 @@ class App extends Component {
             changeOwed = due - paid;
             
         }
-        this.setState({
-          [this.state.result]: Math.floor(changeOwed)
-        });
+
     
+      //trying to update state values so that they can be re-rendered onto the U/I
+        this.setState({
+                [this.state.result]: Math.floor(changeOwed),
+                [this.state.twentyBills]: twentyBills,
+                [this.state.tenBills]: tenBills,
+                [this.state.fiveBills]: fiveBills,
+                [this.state.oneBills]: oneBills,
+                [this.state.quarterCoins]: quarterCoins,
+                [this.state.dimeCoins]: dimeCoins,
+                [this.state.nickelCoins]: nickelCoins,
+                [this.state.pennyCoins]: pennyCoins
+        });
+          
+        //logs of each bill and coin denomination
        console.log("twenties" ,twentyBills);
         console.log("tens" ,tenBills);
         console.log("fives" ,fiveBills);
@@ -105,7 +141,12 @@ class App extends Component {
         console.log("dimes" , dimeCoins);
         console.log("nickels" ,nickelCoins);
         console.log("pennies" ,pennyCoins); 
+    
+    
   }
+  /*** Change handler that is called upon user input that will 
+  update state values to what user has inputted ***/
+
   handleChange(event){
     this.setState({
      [event.target.name] : event.target.value
@@ -120,10 +161,45 @@ class App extends Component {
       <div className="row">
         <div className='col bg-white offcanvas-horizontal-width' id="input-box">
             <p className="fw-bold">Input Values Here:</p>
-            <input type="number" name="amountDue"placeholder='amount due' onChange={this.handleChange} />
+            <input type="number" name="amountDue"placeholder='amount due' onChange={this.handleChange}/>
             <input type="number" placeholder='amount paid' name="amountPaid" onChange={this.handleChange}/>
-            <button id='calculate' onClick={() => this.calculate(this.state.amountDue, this.state.amountPaid)}>Calculate Change</button>
-            
+            <button id='calculate' onClick={(e) => {
+                e.preventDefault();
+                this.calculate(this.state.amountDue, this.state.amountPaid);
+                
+            }}> Calculate Change</button>
+            <br />
+            <p>{this.state.result}</p>
+
+        </div>
+        <div className="col">
+        <table className="table-primary">
+            {/* table to output All bill denomination values via state */}
+            <tbody>
+                <tr>
+                    <td>{this.state.twentyBills}</td>
+                    <td>{this.state.tenBills}</td>
+                    <td>{this.state.fiveBills}</td>
+                    <td>{this.state.oneBills}</td>
+                    <td>{this.state.quarterCoins}</td>
+                    <td>{this.state.dimeCoins}</td>
+                    <td>{this.state.nickelCoins}</td>
+                    <td>{this.state.pennyCoins}</td>
+
+                </tr>
+                <tr>
+                    <th>Twenties</th>
+                    <th>Tens</th>
+                    <th>Fives</th>
+                    <th>Ones</th>
+                    <th>Quarters</th>
+                    <th>Dimes</th>
+                    <th>Nickels</th>
+                    <th>Pennies</th>
+                 </tr>
+
+            </tbody>
+        </table>
         </div>
         <div className="col">
 
